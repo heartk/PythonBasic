6a40faec3ff9e7ccef2ed68a102f99b71e118dc7
rhsund <it_sun@126.com>
2017-06-30 14:56:14 +0800

    new practice , not finished yet !

--- Ex25.py
+++ Ex25.py
@@ -17,4 +17,22 @@ def print_last_word(words):
     word = words.pop(-1)
     print(word)
 
-# todo ...
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words"""
+    words = break_words(sentence)
+    return  sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
+
+
